195f5eab0bbb0c5218d35442d41c1d4fcfceb4a0
larryfast <larry@legible.com>
2023-03-30 16:16:31 -0700

    testing

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 radio.onReceivedNumber(function (receivedNumber) {
     z = receivedNumber % 10
 })
+input.onButtonPressed(Button.A, function () {
+	
+})
 let z = 0
 z = 0
 radio.setGroup(1)
 basic.forever(function () {
+    led.toggle(0, 0)
     basic.showString("" + (z))
     basic.pause(500)
 })
